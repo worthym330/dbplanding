e22b27d59f6358c7058249042fcb29d2075c9352
Worthy M <mandalbasant330@yahoo.com>
2024-12-23 13:54:29 +0530

    added scripts

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import { Metadata } from "next";
 import { ThemeProvider } from "@/components/ThemeProvider";
 import toast, { Toaster } from "react-hot-toast";
 import FloatingButtons from "@/components/floating-button";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -24,8 +25,79 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="icon" href="/logos/LogoBlack.png" />
+        {/* Microsoft Clarity Script */}
+        <Script
+          id="microsoft-clarity"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "pidfken1vb");
+            `,
+          }}
+        />
+        {/* Facebook Pixel Base Code */}
+        <Script
+          id="facebook-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', 'YOUR_PIXEL_ID');
+            fbq('track', 'PageView');
+            `,
+          }}
+        />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=YOUR_PIXEL_ID&ev=PageView&noscript=1"
+          />
+        </noscript>
+        {/* Google Analytics with gtag.js */}
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=GTM-5GS6FK4P`}
+          strategy="afterInteractive"
+        />
+        <Script
+          id="gtag-init"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'GTM-5GS6FK4P', {
+              page_path: window.location.pathname,
+            });
+            `,
+          }}
+        />
+        
       </head>
       <body className={inter.className}>
+        {/* <!-- Google Tag Manager (noscript) --> */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5GS6FK4P"
+            height="0"
+            width="0"
+            style={{ display: "none" }}
+          ></iframe>
+        </noscript>
+        {/* <!-- End Google Tag Manager (noscript) --> */}
         <Toaster position="top-center" reverseOrder={false} />
         <ThemeProvider
           attribute="class"
