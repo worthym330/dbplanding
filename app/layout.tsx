d9a4a25b59425a24e6d768930bbadb9d99f4eb50
Worthy M <mandalbasant330@yahoo.com>
2024-12-23 14:04:43 +0530

    gtag added with analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -85,7 +85,26 @@ export default function RootLayout({
             `,
           }}
         />
-        
+
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=G-RHDYSGZ7XX`}
+          strategy="afterInteractive"
+        />
+
+        <Script
+          id="gtag-init"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-RHDYSGZ7XX', {
+              page_path: window.location.pathname,
+            });
+            `,
+          }}
+        />
       </head>
       <body className={inter.className}>
         {/* <!-- Google Tag Manager (noscript) --> */}
