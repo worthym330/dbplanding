35836de4c4ee295a8361c431c85764e5c5e13af6
Worthy M <mandalbasant330@yahoo.com>
2025-01-01 13:26:41 +0530

    Update hotel list layout to improve responsiveness

--- components/hotel-list.tsx
+++ components/hotel-list.tsx
@@ -28,7 +28,7 @@ export function HotelList() {
     <div>
       <FilterBar onFilterChange={handleFilterChange} hotels={filteredHotels} />
       <div className="mx-auto max-w-7xl px-4 py-12" id="hotels">
-        <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
+        <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
           {filteredHotels
             .sort((a, b) => {
               if (a.ispartner === b.ispartner) return 0;
