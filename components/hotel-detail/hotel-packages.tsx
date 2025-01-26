beb873f842d640c14818ca87eb458abc71dc5723
Worthy M <mandalbasant330@yahoo.com>
2025-01-26 14:23:19 +0530

    Minor change

--- components/hotel-detail/hotel-packages.tsx
+++ components/hotel-detail/hotel-packages.tsx
@@ -245,7 +245,7 @@ export function HotelPackages({ packages, hotel }: HotelPackagesProps) {
               transition={{ delay: index * 0.1 }}
             >
               <Card className="relative">
-                {pkg.stock !== undefined && pkg.stock <= 10 && (
+                {pkg.stock !== undefined && pkg.stock < 10 && (
                   <div className="absolute -top-2 left-1/2 transform -translate-x-1/2 bg-red-500 text-white text-xs font-bold py-1 px-3 rounded-full">
                     {pkg.stock > 0
                       ? `Only ${pkg.stock} packages left!`
